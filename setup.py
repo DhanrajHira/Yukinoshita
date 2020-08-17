578cc3312a60253edfc0dc6fca64583ca623f945
Dhanraj Hira <hiradhanraj0100@gmail.com>
2020-08-17 19:42:10 +0530

    Incomplete setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yukinoshita",
+    version="0.3.0",
+    license="MIT",
+    author="Dhanraj Hira, Not Marek",
+    author_email="hiradhanraj0100@gmail.com",
+    description="A simple HLS playlist downloader.",
+    long_description=open("README.md", "r").read(),
+    long_description_content="text/markdown",
+    url="https://github.com/DhanrajHira/Yukinoshita",
+    packages=find_packages(),
+     classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+    
+    ]   
+)
