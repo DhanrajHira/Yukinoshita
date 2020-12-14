a173a40d9de2384ec14c526645712a35072ed5d7
Dhanraj Hira <hiradhanraj0100@gmail.com>
2020-12-14 20:42:19 +0530

    Rewrote the Downloader class to be async.

--- Yukinoshita/downloader.py
+++ Yukinoshita/downloader.py
@@ -1,250 +1,87 @@
+import aiofile
+import aiohttp
+import asyncio
+import m3u8
+from Crypto.Cipher import AES
+import typing
 import os
-from threading import Lock
-from progress.bar import IncrementalBar
-from Yukinoshita.progress_tracker import ProgressTracker
-from Yukinoshita.decrypter_provider import DecrypterProvider
-from Yukinoshita.merger import FFmpegMerger, ChunkMerger, ChunkRemover
-from concurrent.futures import ThreadPoolExecutor
+
+async def _worker(queue: asyncio.Queue, client: aiohttp.ClientSession):
+    while True:
+        try:
+            segment_data = await queue.get()
+            file_name: str = segment_data[0]
+            segment: typing.List[m3u8.Segment] = segment_data[1]
+            resp: aiohttp.ClientResponse = await client.get(segment.uri)
+            resp_data: bytes = await resp.read()
+
+            if segment.key is not None and segment.key !='':
+                key_resp = await client.get(segment.key.uri)
+                key = await key_resp.read()
+                print("Decrypt key received.")
+                data_to_write = AES.new(key, AES.MODE_CBC).decrypt(resp_data)
+                print("Decrypted")
+            else:
+                data_to_write = resp_data
+            async with aiofile.async_open(file_name, "wb+") as file:
+                await file.write(data_to_write)
+            print("Written")
+            queue.task_done()
+        except: 
+            print("Quitting")
 
 class Downloader(object):
-    """
-    Facilitates downloading an episode from aniwatch.me using a single thread.
+    def __init__(self, m3u8_str: str, output_file_name: str, max_workers: int = 3) -> None:
+        self._m3u8: m3u8.M3U8 = m3u8.M3U8(m3u8_str)
+        self._max_workers = max_workers
+        self._output_file_name = output_file_name
 
-    """
-    def __init__(
-        self,
-        network,
-        m3u8,
-        file_name: str,
-        id: int,
-        use_ffmpeg: bool = True,
-        delete_chunks: bool = True,
-        hooks = {},
-    ):
-        self.__network__ = network
-        self.m3u8 = m3u8
-        self.file_name = file_name
-        self.id = id
-        self.use_ffmpeg = use_ffmpeg
-        self.delete_chunks = delete_chunks
-        self.hooks = hooks
-        self.progress_tracker = ProgressTracker(id)
-
-    def init_tracker(self, chunk_tuple_list, decrypter_provider):
-        self.progress_tracker.init_tracker(
-            {
-                "network": self.__network__,
-                "decrypter_provider": decrypter_provider,
-                "chunk_tuple_list": chunk_tuple_list,
-                "file_name": self.file_name,
-                "total_chunks": self.total_chunks,
-            }
-        )
-
-    def download(self):
-        chunk_tuple_list = []
-        # Will hold a list of tuples of the form (chunk_number, chunk).
-        # The chunk_number in this list will start from 1.
-        for chunk_number, chunk in enumerate(self.m3u8.data["segments"]):
-            chunk_tuple_list.append((chunk_number, chunk))
-
-        if self.hooks.get("modify_chunk_tuple_list", None) is not None:
-            chunk_tuple_list = self.hooks["modify_chunk_tuple_list"].__call__(chunk_tuple_list)
-
-        self.total_chunks = len(chunk_tuple_list)
-
-        try:
-            os.makedirs("chunks")
-        except FileExistsError:
-            pass
-
-        self.progress_bar = IncrementalBar("Downloading", max=self.total_chunks)
-        decrypter_provider = DecrypterProvider(self.__network__, self.m3u8)
-        self.init_tracker(chunk_tuple_list, decrypter_provider)
-        for chunk_number, chunk_tuple in enumerate(chunk_tuple_list):
-            # We need the chunk number here to name the files. Note that this is
-            # different from the chunk number that is inside the tuple.
-            file_name = f"chunks\/{self.file_name}-{chunk_number}.chunk.ts"
-            ChunkDownloader(
-                self.__network__,
-                chunk_tuple[1], # The segment data
-                file_name,
-                chunk_tuple[0], # The chunk number needed for decryption.
-                decrypter_provider
-                ).download()
-            self.progress_bar.next()
-            self.progress_tracker.update_chunks_done(chunk_number)
-            if self.hooks.get("on_progress", None) is not None:
-                self.hooks["on_progress"].__call__(chunk_number + 1, self.total_chunks)
-
-        self.progress_bar.finish()
-
-    def merge(self):
-        """Merges the downloaded chunks into a single file.
-        """
-        if self.use_ffmpeg:
-            FFmpegMerger(self.file_name, self.total_chunks).merge()
-        else:
-            ChunkMerger(self.file_name, self.total_chunks).merge()
-
-    def remove_chunks(self):
-        """Deletes the downloaded chunks.
-        """
-        ChunkRemover(self.file_name, self.total_chunks).remove()
-
-
-class ChunkDownloader(object):
-    """
-    The object that actually downloads a single chunk.
-    """
-    def __init__(self, network, segment, file_name, chunk_number, decrypt_provider: DecrypterProvider):
-        self.__network = network
-        self.segment = segment
-        self.file_name = file_name
-        self.chunk_number = chunk_number,
-        self.decrypter_provider = decrypt_provider
-
-    def download(self):
-        """Starts downloading the chunk.
-        """
-        with open(self.file_name, "wb") as videofile:
-            res = self.__network.get(self.segment["uri"])
-            chunk = res.content
-            key_dict = self.segment.get("key", None)
-            #This is done to check if the chunk in question is encrypted. 
-            
-            if key_dict is not None:
-                #If the chunk is encrypted it will have a key dict.
-                decrypted_chunk = self.decrypt_chunk(chunk)
-                videofile.write(decrypted_chunk)
-            else:
-                videofile.write(chunk)
-    
-    def decrypt_chunk(self, chunk):
-        decryter = self.decrypter_provider.get_decrypter(self.chunk_number)
-        return decryter.decrypt(chunk)
-
-
-class MultiThreadDownloader(object):
-    """
-    Facilitates downloading an episode from aniwatch.me using multiple threads.
-    """
-    def __init__(
-        self,
-        network,
-        m3u8,
-        file_name: str,
-        id: int,
-        max_threads: int = None,
-        use_ffmpeg: bool = True,
-        hooks = {},
-        delete_chunks: bool = True,
-    ):
-        self.__network = network
-        self.m3u8 = m3u8
-        self.file_name = file_name
-        self.use_ffmpeg = use_ffmpeg
-        self.max_threads = max_threads
-        self.delete_chunks = delete_chunks
-        self.hooks = hooks
-        self.id = id
-        self.progress_tracker = ProgressTracker(id)
-        self.__lock = Lock()
-      
-        try:
-            os.makedirs("chunks")
-        except FileExistsError:
-            pass
-
-    def init_tracker(self):
-        self.progress_tracker.init_tracker(
-            {
-                "headers": None,
-                "cookies": None,
-                "segments": self.m3u8.data["segments"],
-                "file_name": self.file_name,
-                "total_chunks": self.total_chunks,
-            }
-        )
-
-
-    def assign_segments(self, segment):
-
-        ChunkDownloader(
-            segment.network,
-            segment.segment,
-            segment.file_name,
-            segment.chunk_number,
-            segment.decrypter_provider
-        ).download()
-        with self.__lock:
-            self.progress_tracker.update_chunks_done(segment.chunk_number)
-            self.progress_bar.next()
-
-    def download(self):
-        """Runs the downloader and starts downloading the video file.
-        """
-
-        decrypter_provider = DecrypterProvider(self.__network, self.m3u8)
-        chunk_tuple_list = []
-        # Will hold a list of tuples of the form (chunk_number, chunk).
-        # The chunk_number in this list will start from 0.
-        for chunk_number, chunk in enumerate(self.m3u8.data["segments"]):
-            chunk_tuple_list.append((chunk_number, chunk))
-
-        if self.hooks.get("modify_chunk_tuple_list", None) is not None:
-            chunk_tuple_list = self.hooks["modify_chunk_tuple_list"].__call__(chunk_tuple_list)
-
-        self.total_chunks = len(chunk_tuple_list)
-        self.progress_bar = IncrementalBar("Downloading", max=self.total_chunks)
-        #self.init_tracker()
-
-        segment_wrapper_list = []
-
-        for chunk_number, chunk in enumerate(chunk_tuple_list):
-            file_name = f"chunks\/{self.file_name}-{chunk_number}.chunk.ts"
-            segment_wrapper = _SegmentWrapper(
-                self.__network,
-                chunk[1], # Segment data.
-                file_name,
-                chunk[0], # The chunk number needed for decryption.
-                decrypter_provider
-            )
-            segment_wrapper_list.append(segment_wrapper)
-
-        if self.max_threads == None:
-            # If the value for max threads is not provided, then it is set to 
-            # the total number of chunks that are to be downloaded.
-            self.max_threads = self.total_chunks
-
-        self.executor = ThreadPoolExecutor(max_workers = self.max_threads)
+    async def run(self):
+        queue = asyncio.Queue()
+        client = aiohttp.ClientSession()
         
-        with self.executor as exe:
-            futures = exe.map(self.assign_segments, segment_wrapper_list)
-            for future in futures: 
-                # This loop servers to run the generator.
-                pass
-        self.progress_bar.finish()
-
-    def merge(self):
-        """Merges the downloaded chunks into a single file.
-        """
-        if self.use_ffmpeg:
-            FFmpegMerger(self.file_name, self.total_chunks).merge()
+        if len(self._m3u8.playlists):
+            stream_uri = max(*self._m3u8.playlists, key=lambda p: p.stream_info.bandwidth).uri
+            resp = await client.get(stream_uri)
+            stream = m3u8.M3U8(await resp.text()) 
         else:
-            ChunkMerger(self.file_name, self.total_chunks).merge()
+            stream = self._m3u8
+        
+        try:
+            os.makedirs("segments")
+        except FileExistsError:
+            pass
 
-    def remove_chunks(self):
-        """Deletes the downloaded chunks.
-        """
-        ChunkRemover(self.file_name, self.total_chunks).remove()
+        async with aiofile.async_open(os.path.join("segments", "index.yk"), "w+") as file:
+            await file.write("test")
+        
+        for segment_number, segment in enumerate(stream.segments):
+            await queue.put((
+                os.path.join("segments",f"{self._output_file_name}-{segment_number}.chunk"),
+                segment
+                ))
 
-class _SegmentWrapper(object):
-    # As the name suggests, this is only wrapper class introduced with a hope that it 
-    # will lead to more readable code.
-    def __init__(self, network, segment, file_name, chunk_number, decrypter_provider):
-        self.network = network
-        self.segment = segment
-        self.file_name = file_name
-        self.chunk_number = chunk_number
-        self.decrypter_provider = decrypter_provider
+        workers = [
+            asyncio.create_task(_worker(queue, client))
+            for _ in range(self._max_workers)
+        ]
+        
+        await queue.join()
+
+        for worker in workers:
+            worker.cancel()
+        
+        await client.close()
+
+    @classmethod
+    async def from_url(cls, url, output_file_name, max_workers=5):
+        client = aiohttp.ClientSession()
+        resp = await client.get(url)
+        resp_text = await resp.text()
+        await client.close()
+        return cls(resp_text, output_file_name, max_workers)
+
+async def main():
+    d = await Downloader.from_url("https://vengeance.animex.vip/GYJQV73V6/episodes/2/master.m3u8", "demo", 20)
+    await d.run()
+asyncio.run(main())
